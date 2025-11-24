fe89cb5363e4af2931f0b85c44bddbaa9338c3bc
k.grinevich.19234@gmail.com <kyrylo.hrynevych@etu-upsaclay.fr>
2025-11-24 01:09:50 +0100

    Ajout de qqs fonctions de vérfication de base sur les types et du typage des expressions

--- typechecker.ml
+++ typechecker.ml
@@ -18,11 +18,73 @@ module Env = Map.Make(String)
 type tenv = typ Env.t
 type fenv = (typ list) * (typ list) Env.t
 type senv = (ident * typ) list
+type fdef = func_def
+
+let dloc : location = (Lexing.dummy_pos, Lexing.dummy_pos)
 
 let dummy = "_"
 
 let add_env l tenv =
-  List.fold_left (fun env (x, t) -> if x = dummy then env else Env.add x t env) tenv l
+  List.fold_left (fun env (x, t) -> if x.id = dummy then env else Env.add x.id   t env) tenv l
+
+let equal_typ (a : typ) (b : typ) : bool =
+  match a, b with
+  | TInt, TInt | TBool, TBool | TString, TString -> true
+  | TStruct sa, TStruct sb -> sa = sb
+  | _ -> false
+
+(* Vérifs de base sur les types / champs *)
+
+let check_typ (senv : senv) (t : typ) : unit =
+  match t with
+  | TInt | TBool | TString -> ()
+  | TStruct s ->
+      if not (Env.mem s senv) then
+        error dloc (Printf.sprintf "unknown struct %s" s)
+
+let check_fields (senv : senv) (lf : (ident * typ) list) : unit =
+  let seen = Hashtbl.create 16 in
+  List.iter
+    (fun (id, t) ->
+       if Hashtbl.mem seen id.id then
+         error id.loc (Printf.sprintf "duplicate field %s" id.id);
+       Hashtbl.add seen id.id ();
+       check_typ senv t)
+    lf
+
+let lookup_field (fields : (ident * typ) list) (fname : string) : (typ * location) =
+  let rec aux = function
+    | [] -> raise Not_found
+    | (id, t) :: q -> if id.id = fname then (t, id.loc) else aux q
+  in
+  aux fields
+
+(* ---------- Typage des expressions ---------- *)
+
+let rec check_expr ~(fmt_imported:bool) (senv:senv) (fenv:fenv) (tenv:tenv) (e:expr) (expected:typ) : unit =
+  match e.edesc with
+  | Nil ->
+      (match expected with
+       | TStruct _ -> ()
+       | _ -> error e.eloc "nil can only be used for struct/pointer types")
+  | _ ->
+      let te = type_expr ~fmt_imported senv fenv tenv e in
+      if not (equal_typ te expected) then type_error e.eloc te expected
+
+and type_expr ~(fmt_imported:bool) (senv:senv) (fenv:fenv) (tenv:tenv) (e:expr) : typ =
+  match e.edesc with
+  | Int _    -> TInt
+  | Bool _   -> TBool
+  | String _ -> TString
+  | Nil      -> error e.eloc "untyped nil requires a target type"
+  | New s ->
+      if not (Env.mem s senv) then error e.eloc (Printf.sprintf "unknown struct %s" s);
+      TStruct s
+  | Var id ->
+      (match Env.find_opt id.id tenv with
+       | Some t -> t
+       | None -> error id.loc (Printf.sprintf "undefined variable %s" id.id))
+  | Dot (ex, fid) -> ...
 
 let prog (fmt,ld) =
   (* collecte les noms des fonctions et des structures sans les vérifier *)
